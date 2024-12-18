7203102a21f3e09a7b13d09dca746b510743cf23
Anand Tiwari <167313054+atsrf2@users.noreply.github.com>
2024-12-18 13:41:39 +0530

    2nd commit

---
+++ variable.tf
@@ -0,0 +1,25 @@
+variable "subscription_id" {
+  type        = string
+  default     = ""
+  description = "description"
+}
+
+variable "client_id" {
+  type        = string
+  default     = ""
+  description = "description"
+}
+
+variable "client_secret" {
+  type        = string
+  default     = ""
+  description = "description"
+}
+
+variable "tenant_id" {
+  type        = string
+  default     = ""
+  description = "description"
+}
+
+
